a472d4cab615294059663c7c3843016b2c98d944
Bowie <marketing@lestcode.com.br>
2024-09-11 12:13:56 -0300

    fundamentos 002

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-// olá, mundo
-const mensagem = "olá eu"
-
-{
-   const mensagem = "olá, mundo!"
-   console.log(mensagem);
+// arrays, objetos
+let meta = {
+    value: 'ler um livro por mês',
+    address: 2,
+    checked:  false,
+    log: (info) => {
+       console.log(info)
+    }
 }
-console.log(mensagem);
+meta.value = "mão é mais ler um livro"
+meta.log(meta.value) 
+
+// function // arrow function
+//const criarMeta = () => {}
+
+// named function
+//function criarMetas() {}
