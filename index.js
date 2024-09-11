5bd14ff9d3ad8ec87200b73d7ffcea7a0a110e86
Bowie <marketing@lestcode.com.br>
2024-09-11 12:17:58 -0300

    fundamentos 003

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 // arrays, objetos
 let meta = {
     value: 'ler um livro por mês',
-    address: 2,
-    checked:  false,
-    log: (info) => {
-       console.log(info)
-    }
+    checked:  true,
 }
-meta.value = "mão é mais ler um livro"
-meta.log(meta.value) 
 
-// function // arrow function
-//const criarMeta = () => {}
+let metas = [
+    meta,
+    {
+        value: 'caminhar 20 minutos todos os dias',
+        checked: false
+    }
+]
 
-// named function
-//function criarMetas() {}
+console.log(metas[1].value);
+
+
